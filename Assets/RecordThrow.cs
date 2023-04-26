abe81017fe5ce3b7c6074242c46ad6b8e4334f51
RJ <soaringrocstudio@gmail.com>
2023-04-25 20:02:01 -0500

    Textures applied and exploding / sticking discs

--- Assets/RecordThrow.cs
+++ Assets/RecordThrow.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class RecordThrow : MonoBehaviour
 {
+    public GameObject wallSavePrefab;
+    public GameObject wallBreakPrefab;
+
     public LayerMask layerMask;
 
     public Transform headTransform;
 
     public float throwForceAdjust;
 
+    private bool _thrown;
     private Rigidbody _rb;
+
+    public bool Thrown()
+    {
+        return _thrown;
+    }
 
     public void Start()
     {
@@ -25,7 +34,12 @@ public class RecordThrow : MonoBehaviour
     [ContextMenu("Editor Throw")]
     public void ThrowForce()
     {
-        if (_rb)
+        if (!headTransform)
+        {
+            headTransform = Camera.main.transform;
+        }
+
+        if (_rb && headTransform && !_thrown)
         {
             //_rb.Sleep();
 
@@ -38,8 +52,40 @@ public class RecordThrow : MonoBehaviour
                 //TODO: Add force adjustment based on wrist speed
 
                 _rb.AddForce(hitDir * throwForceAdjust, ForceMode.Impulse);
+
+                _thrown = true;
             }
 
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_thrown)
+        {
+            if (collision.gameObject.tag == "SaveWall")
+            {
+                GameObject saveDisc = Instantiate(wallSavePrefab, transform.position, transform.rotation);
+
+                Vector3 wallPos = collision.collider.ClosestPoint(transform.position);
+
+                saveDisc.transform.position = wallPos;
+
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                GameObject breakDisc = Instantiate(wallBreakPrefab, transform.position, transform.rotation);
+
+                Rigidbody[] rbs = breakDisc.GetComponentsInChildren<Rigidbody>();
+
+                foreach(Rigidbody rb in rbs)
+                {
+                    rb.AddExplosionForce(.75f, transform.position, 1f, .075f, ForceMode.Impulse);
+                }
+
+                gameObject.SetActive(false);
+            }
         }
     }
 }
