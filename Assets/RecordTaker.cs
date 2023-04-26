ce24b002a24127f723481014a9dae792cc929353
RJ <soaringrocstudio@gmail.com>
2023-04-26 12:39:22 -0500

    Sounds and music playing

--- Assets/RecordTaker.cs
+++ Assets/RecordTaker.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class RecordTaker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource playerAudio;
+
+    public Transform needle;
+    public float needleAngle, needleAngleRequired;
+    public bool xrequire, yrequire, zrequire;
+
+    private bool canPlay;
+
+    public void PlaySong(AudioClip clip)
     {
-        
+        playerAudio.clip = clip;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (xrequire)
+        {
+            if (needle.localEulerAngles.x > needleAngleRequired)
+            {
+                canPlay = true;
+            }
+            else
+            {
+                canPlay = false;
+            }
+        }
+
+        if (yrequire)
+        {
+            if (needle.localEulerAngles.y > needleAngleRequired)
+            {
+                canPlay = true;
+            }
+            else
+            {
+                canPlay = false;
+            }
+        }
+
+        if (zrequire)
+        {
+            if (needle.localEulerAngles.z > needleAngleRequired)
+            {
+                canPlay = true;
+            }
+            else
+            {
+                canPlay = false;
+            }
+        }
+
+        if (canPlay && !playerAudio.isPlaying && playerAudio.clip != null)
+        {
+            playerAudio.Play();
+        }
+
+        if (playerAudio.clip == null && playerAudio.isPlaying)
+        {
+            playerAudio.Stop();
+        }
+
+        if (!canPlay)
+        {
+            playerAudio.Stop();
+        }
     }
 }
