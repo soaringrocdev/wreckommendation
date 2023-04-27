533ccbc904ab0301b9c2cec0838242738b86a8eb
Jeremy K <jeremyk@xanu.com>
2023-04-27 11:44:04 -0700

    updated handler to push through the first ui screen to the second on click

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+public GameObject uiholder;
+
 public class UIHandler : MonoBehaviour
 {
 
 public class ClickAction : MonoBehaviour, IPointerClickHandler {
         public void OnPointerClick(PointerEventData eventData) {
+            if(this.transform.name = "Winning1")
+                {
+                    uiholder = gameObject.Find("Winning2");
+                    this.gameObject.SetActive(false);
+                    uiholder.setActive(true);
+                }
+            else {
                 this.gameObject.SetActive(false);
         }
     }
