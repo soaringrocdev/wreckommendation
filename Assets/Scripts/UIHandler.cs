6ba7ca61c02f3d5b5d8201d0facf3e192398b43e
Jeremy K <jeremyk@xanu.com>
2023-04-27 11:44:47 -0700

    fixed bug with uihandler

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -10,14 +10,13 @@ public class UIHandler : MonoBehaviour
 
 public class ClickAction : MonoBehaviour, IPointerClickHandler {
         public void OnPointerClick(PointerEventData eventData) {
-            if(this.transform.name = "Winning1")
-                {
-                    uiholder = gameObject.Find("Winning2");
-                    this.gameObject.SetActive(false);
-                    uiholder.setActive(true);
-                }
-            else {
+            if (this.transform.name = "Winning1") {
+                uiholder = gameObject.Find("Winning2");
                 this.gameObject.SetActive(false);
+                uiholder.setActive(true);
+            } else {
+                this.gameObject.SetActive(false);
+            }
         }
     }
 }
